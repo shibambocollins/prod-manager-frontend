1bde9514ad0f7ed5dc603b7e378920a6d820410d
shibambocollins <ntsobokwanec@gmail.com>
2025-12-17 22:42:13 +0200

    added product placehoders

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import { useState } from 'react';
+import ProductList from './components/ProductList';
+
+
 function App() {
+   const [products, setProducts] = useState([]);
+
+  const loadProducts = () => {
+    const fakeProducts = [
+      { id: 1, name: 'Laptop', price: 12000 },
+      { id: 2, name: 'Mouse', price: 250 },
+      { id: 3, name: 'Keyboard', price: 800 }
+    ];
+
+    setProducts(fakeProducts);
+  };
+
   return (
-    <div>
+     <div>
       <h1>Items Manager</h1>
 
-      <button>Add Product</button>
+      <button onClick={loadProducts}>
+        Load Products
+      </button>
 
-      <ul>
-        <li>No products yet</li>
-      </ul>
+      <ProductList products={products} />
     </div>
   );
 }

179b68341dd9dd86295c72c6f59c0bc25853fc99
shibambocollins <ntsobokwanec@gmail.com>
2025-12-19 14:22:18 +0200

    update port

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8080/api/products';
+const BASE_URL = 'http://localhost:8081/api/products';
 
 export const getProducts = async () => {
   const response = await fetch(BASE_URL);
